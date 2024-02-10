6b749ad04c5193ea24162bde2f530a1e6c6b7257
Hugo Saladini <80492463+HugoSaladini@users.noreply.github.com>
2024-02-10 00:06:04 +0000

    atualizar main

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+const { defaultConfig } = require("next/dist/server/config-shared");
+
+function Home() {
+    return <h1>Thalia...🔮qual a proxima aposta que você vai perder? qro meu premio 😘</h1>
+}
+
+export default Home;
