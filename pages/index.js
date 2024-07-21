a09e79c879805981c4148399d6642aef30d031a1
Hugo Saladini <80492463+HugoSaladini@users.noreply.github.com>
2024-07-21 21:50:20 +0000

    page construction

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
 const { defaultConfig } = require("next/dist/server/config-shared");
 
 function Home() {
-  return (
-    <h1>
-      Thalia...🔮qual a proxima aposta que você vai perder? qro meu premio 😘
-    </h1>
-  );
+  return <h1>||🚧🚧 EM CONTRUÇAO 🚧🚧||</h1>;
 }
 
 export default Home;
